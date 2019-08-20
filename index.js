97883b5c738f6ce093339dafaef0da9f9c13c0b9
Ziad EL KHOURY HANNA <ziad.khoury.h@gmail.com>
2019-08-20 12:16:24 +0200

    Refactor the CLI specific code into the CLI module

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-const minimist = require('minimist');
-const {readStdin, readFile, writeFile, walk} = require('./utils.js');
+const {readStdin, readFile} = require('./utils.js');
 const parse = require('./parse');
 
 async function processSource(source, {command, ...options}) {
@@ -12,46 +11,6 @@ async function processStdin(options) {
   return processSource(await readStdin(), options);
 }
 
-const command = process.argv[2];
-const {_: filenames, ...options} = minimist(process.argv.slice(3), {
-  alias: {
-    h: 'help',
-  },
-});
-
-switch (command) {
-case 'wrap':
-case 'unwrap':
-case 'remove':
-  options.command = command;
-  break;
-default:
-  options.command = null;
-}
-
-if (options.h || !options.command) {
-  console.error(`flow-comments (wrap|unwrap|remove) [-h|--help] [--spaceBefore] [--spaceInside] [files...]`);
-  process.exit(1);
-}
-
-options.command = require('./commands/' + options.command + '.js');
-
-if (filenames.length > 0) {
-  walk(filenames).map(async function(filename) {
-    try {
-      const result = await processFile(filename, options);
-      await writeFile(filename, result);
-    } catch (error) {
-      console.error(filename, error);
-    }
-  });
-} else {
-  processStdin(options)
-    .then(function(result) {
-      process.stdout.write(result);
-    })
-    .catch(function(error) {
-      console.error(error);
-      process.exit(1);
-    });
-}
+exports.processSource = processSource;
+exports.processFile = processFile;
+exports.processStdin = processStdin;
