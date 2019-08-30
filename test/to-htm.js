63fd801ab8e87ec59b57f938cf24a5122556c7de
Ziad EL KHOURY HANNA <ziad.khoury.h@gmail.com>
2019-08-30 15:58:34 +0200

    Split the test.js file

--- test/to-htm.js
+++ test/to-htm.js
@@ -1,74 +1,26 @@
-import fs from 'fs';
 import test from 'ava';
 import { processSource } from '../src/main.js';
-
-function readFile(filename) {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(filename, 'utf8', function(err, code) {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(code);
-    });
-  });
-}
-
-test('wrap - string', async t => {
-  t.is(
-    await processSource('const a: string = 2', { command: 'wrap' }),
-    'const a/*: string*/ = 2',
-  );
-});
-
-test('wrap - file', async t => {
-  const unwrappedSource = await readFile(`${__dirname}/fixtures/unwrapped.js`);
-  const wrappedSource = await readFile(`${__dirname}/fixtures/wrapped.js`);
-
-  t.is(
-    await processSource(unwrappedSource, { command: 'wrap' }),
-    wrappedSource,
-  );
-});
-
-test('unwrap - string', async t => {
-  t.is(
-    await processSource('const a/*: string*/ = 2', { command: 'unwrap' }),
-    'const a: string = 2',
-  );
-});
-
-test('unwrap - file', async t => {
-  const unwrappedSource = await readFile(`${__dirname}/fixtures/unwrapped.js`);
-  const wrappedSource = await readFile(`${__dirname}/fixtures/wrapped.js`);
-
-  t.is(
-    await processSource(wrappedSource, { command: 'unwrap' }),
-    unwrappedSource,
-  );
-});
-
-/*eslint-disable no-template-curly-in-string*/
 
 function compile(code, options) {
   return processSource(code, { command: 'to-htm', ...options });
 }
 
-test('to-htm - import - import shortcut', async t => {
+/*eslint-disable no-template-curly-in-string*/
+test('import - import shortcut', async t => {
   t.is(
     await compile('(<div />);', { import: 'htm/preact' }),
     'import {html} from "htm/preact";\n(html`<div />`);',
   );
 });
 
-test('to-htm - import - import shortcut, dotted tag', async t => {
+test('import - import shortcut, dotted tag', async t => {
   t.is(
     await compile('(<div />);', { tag: 'html.bound', import: 'htm/preact' }),
     'import {html} from "htm/preact";\n(html.bound`<div />`);',
   );
 });
 
-test('to-htm - import - named import', async t => {
+test('import - named import', async t => {
   t.is(
     await compile('(<div />);', {
       import: { module: 'htm/preact', export: '$html' },
@@ -77,7 +29,7 @@ test('to-htm - import - named import', async t => {
   );
 });
 
-test('to-htm - import - named import, dotted tag', async t => {
+test('import - named import, dotted tag', async t => {
   t.is(
     await compile('(<div />);', {
       tag: 'html.bound',
@@ -87,7 +39,7 @@ test('to-htm - import - named import, dotted tag', async t => {
   );
 });
 
-test('to-htm - import - default import', async t => {
+test('import - default import', async t => {
   t.is(
     await compile('(<div />);', {
       import: { module: 'htm/preact', export: 'default' },
@@ -96,7 +48,7 @@ test('to-htm - import - default import', async t => {
   );
 });
 
-test('to-htm - import - namespace import', async t => {
+test('import - namespace import', async t => {
   t.is(
     await compile('(<div />);', {
       import: { module: 'htm/preact', export: '*' },
@@ -105,18 +57,18 @@ test('to-htm - import - namespace import', async t => {
   );
 });
 
-test('to-htm - import - no import without JSX', async t => {
+test('import - no import without JSX', async t => {
   t.is(await compile('false;', { import: 'htm/preact' }), 'false;');
 });
 
-test('to-htm - import - import before comment when no imports found', async t => {
+test('import - import before comment when no imports found', async t => {
   t.is(
     await compile('/**/\n(<div />);', { import: 'htm/preact' }),
     'import {html} from "htm/preact";\n/**/\n(html`<div />`);',
   );
 });
 
-test('to-htm - import - import after comment when imports found', async t => {
+test('import - import after comment when imports found', async t => {
   t.is(
     await compile('/**/\nimport Foo from "foo";\n(<div />);', {
       import: 'htm/preact',
@@ -125,7 +77,7 @@ test('to-htm - import - import after comment when imports found', async t => {
   );
 });
 
-test('to-htm - import - no import if already in scope', async t => {
+test('import - no import if already in scope', async t => {
   t.is(
     await compile('import html from "foo";\n(<div />);', {
       import: 'htm/preact',
@@ -134,7 +86,7 @@ test('to-htm - import - no import if already in scope', async t => {
   );
 });
 
-test('to-htm - elements and text - single named element', async t => {
+test('elements and text - single named element', async t => {
   t.is(await compile('(<div />);'), '(html`<div />`);');
 
   t.is(await compile('(<div>a</div>);'), '(html`<div>a</div>`);');
@@ -148,7 +100,7 @@ test('to-htm - elements and text - single named element', async t => {
   t.is(await compile('(<div_>a</div_>);'), '(html`<div_>a</div_>`);');
 });
 
-test('to-htm - elements and text - single component element', async t => {
+test('elements and text - single component element', async t => {
   t.is(await compile('(<Foo />);'), '(html`<${Foo} />`);');
 
   t.is(await compile('(<Foo>a</Foo>);'), '(html`<${Foo}>a</${Foo}>`);');
@@ -170,13 +122,13 @@ test('to-htm - elements and text - single component element', async t => {
   t.is(await compile('(<$foo>a</$foo>);'), '(html`<${$foo}>a</${$foo}>`);');
 });
 
-test('to-htm - elements and text - dotted component element', async t => {
+test('elements and text - dotted component element', async t => {
   t.is(await compile('(<a.b.c />);'), '(html`<${a.b.c} />`);');
 
   t.is(await compile('(<a.b.c>a</a.b.c>);'), '(html`<${a.b.c}>a</${a.b.c}>`);');
 });
 
-test('to-htm - elements and text - static text', async t => {
+test('elements and text - static text', async t => {
   t.is(await compile('(<div>Hello</div>);'), '(html`<div>Hello</div>`);');
   t.is(
     await compile('(<div>こんにちわ</div>);'),
@@ -184,18 +136,18 @@ test('to-htm - elements and text - static text', async t => {
   );
 });
 
-test('to-htm - elements and text - HTML entities get unescaped', async t => {
+test('elements and text - HTML entities get unescaped', async t => {
   t.is(await compile('(<div>&amp;</div>);'), '(html`<div>&</div>`);');
 });
 
-test('to-htm - elements and text - &lt; gets wrapped into an expression container', async t => {
+test('elements and text - &lt; gets wrapped into an expression container', async t => {
   t.is(
     await compile('(<div>a&lt;b&lt;&lt;&lt;c</div>);'),
     '(html`<div>${"a<b<<<c"}</div>`);',
   );
 });
 
-test('to-htm - fragments - React.Fragment', async t => {
+test('fragments - React.Fragment', async t => {
   t.is(
     await compile(
       '<React.Fragment><div>Foo</div><div>Bar</div></React.Fragment>',
@@ -204,21 +156,21 @@ test('to-htm - fragments - React.Fragment', async t => {
   );
 });
 
-test('to-htm - fragments - short syntax', async t => {
+test('fragments - short syntax', async t => {
   t.is(
     await compile('<><div>Foo</div><div>Bar</div></>'),
     'html`<div>Foo</div><div>Bar</div>`',
   );
 });
 
-test('to-htm - fragments - root expressions', async t => {
+test('fragments - root expressions', async t => {
   t.is(
     await compile('<React.Fragment>{Foo}{Bar}</React.Fragment>'),
     'html`${Foo}${Bar}`',
   );
 });
 
-test('to-htm - props - static values', async t => {
+test('props - static values', async t => {
   t.is(
     await compile('(<div a="a" b="bb" c d />);'),
     '(html`<div a="a" b="bb" c d />`);',
@@ -229,30 +181,30 @@ test('to-htm - props - static values', async t => {
   );
 });
 
-test('to-htm - props - HTML entities get unescaped', async t => {
+test('props - HTML entities get unescaped', async t => {
   t.is(await compile('(<div a="&amp;" />);'), '(html`<div a="&" />`);');
 });
 
-test('to-htm - props - double quote values with single quotes', async t => {
+test('props - double quote values with single quotes', async t => {
   t.is(await compile('(<div a="\'b\'" />);'), '(html`<div a="\'b\'" />`);');
 });
 
-test('to-htm - props - single quote values with double quotes', async t => {
+test('props - single quote values with double quotes', async t => {
   t.is(await compile('(<div a=\'"b"\' />);'), '(html`<div a=\'"b"\' />`);');
 });
 
-test('to-htm - props - escape values with newlines as expressions', async t => {
+test('props - escape values with newlines as expressions', async t => {
   t.is(await compile('(<div a="\n" />);'), '(html`<div a=${`\n`} />`);');
 });
 
-test('to-htm - props - escape values with both single and double quotes as expressions', async t => {
+test('props - escape values with both single and double quotes as expressions', async t => {
   t.is(
     await compile('(<div a="&#34;\'" />);'),
     '(html`<div a=${"\\"\'"} />`);',
   );
 });
 
-test('to-htm - props - expression values', async t => {
+test('props - expression values', async t => {
   t.is(
     await compile(
       'const Foo = (props, a) => <div a={a} b={"b"} c={{}} d={props.d} e />;',
@@ -261,7 +213,7 @@ test('to-htm - props - expression values', async t => {
   );
 });
 
-test('to-htm - props - spread', async t => {
+test('props - spread', async t => {
   t.is(
     await compile('const Foo = props => <div {...props} />;'),
     'const Foo = props => html`<div ...${props} />`;',
@@ -272,7 +224,7 @@ test('to-htm - props - spread', async t => {
   t.is(await compile('(<div a {...b} c />);'), '(html`<div a ...${b} c />`);');
 });
 
-test('to-htm - nesting - element children are merged into one template', async t => {
+test('nesting - element children are merged into one template', async t => {
   t.is(
     await compile(
       'const Foo = () => <div class="foo" draggable>\n  <h1>Hello</h1>\n  <p>world.</p>\n</div>;',
@@ -281,7 +233,7 @@ test('to-htm - nesting - element children are merged into one template', async t
   );
 });
 
-test('to-htm - nesting - inter-element whitespace is collapsed similarly to the JSX plugin', async t => {
+test('nesting - inter-element whitespace is collapsed similarly to the JSX plugin', async t => {
   t.is(
     await compile(
       'const Foo = props => <div a b> a \n <em> b \n B </em> c <strong> d </strong> e </div>;',
@@ -290,7 +242,7 @@ test('to-htm - nesting - inter-element whitespace is collapsed similarly to the 
   );
 });
 
-test('to-htm - nesting - nested JSX Expressions produce nested templates', async t => {
+test('nesting - nested JSX Expressions produce nested templates', async t => {
   t.is(
     await compile(
       'const Foo = props => <ul>{props.items.map(item =>\n  <li>\n    {item}\n  </li>\n)}</ul>;',
@@ -299,10 +251,9 @@ test('to-htm - nesting - nested JSX Expressions produce nested templates', async
   );
 });
 
-test('to-htm - nesting - empty expressions are ignored', async t => {
+test('nesting - empty expressions are ignored', async t => {
   t.is(
     await compile('(<div>{/* a comment */}</div>);'),
     '(html`<div>${""/* a comment */}</div>`);',
   );
 });
-/*eslint-enable no-template-curly-in-string*/
---
+++ test/wrap.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import test from 'ava';
+import { processSource } from '../src/main.js';
+
+function readFile(filename) {
+  return new Promise(function(resolve, reject) {
+    fs.readFile(filename, 'utf8', function(err, code) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(code);
+    });
+  });
+}
+
+test('string', async t => {
+  t.is(
+    await processSource('const a: string = 2', { command: 'wrap' }),
+    'const a/*: string*/ = 2',
+  );
+});
+
+test('file', async t => {
+  const unwrappedSource = await readFile(`${__dirname}/fixtures/unwrapped.js`);
+  const wrappedSource = await readFile(`${__dirname}/fixtures/wrapped.js`);
+
+  t.is(
+    await processSource(unwrappedSource, { command: 'wrap' }),
+    wrappedSource,
+  );
+});
