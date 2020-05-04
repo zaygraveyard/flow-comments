f38e55ef349e4e1dcfc37007ac6e767d4df47d3e
Ziad EL KHOURY HANNA <ziad.khoury.hanna@oceandatalab.com>
2020-05-05 01:41:53 +0200

    Add tests for wrapping then unwrapping

---
+++ test/wrap_unwrap.js
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import test from 'ava';
+import { processSource } from '../src/main.js';
+
+const spaceInside = true;
+
+function readFile(filename) {
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filename, 'utf8', function (err, code) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(code);
+    });
+  });
+}
+async function wrapUnwrap(source, options) {
+  return processSource(
+    await processSource(source, { command: 'wrap', ...options }),
+    { command: 'unwrap', ...options },
+  );
+}
+async function wrapUnwrap2(source, options) {
+  return wrapUnwrap(await wrapUnwrap(source, options), options);
+}
+async function same(t, fn, source, options) {
+  t.is(await fn(source, options), source);
+}
+
+test('string - wrap -> unwrap', async (t) => {
+  const source = 'const a: string = 2';
+
+  await same(t, wrapUnwrap, source);
+  await same(t, wrapUnwrap, source, { spaceInside });
+});
+
+test('string - (wrap -> unwrap) x2', async (t) => {
+  const source = 'const a: string = 2';
+
+  await same(t, wrapUnwrap2, source);
+  await same(t, wrapUnwrap2, source, { spaceInside });
+});
+
+test('file - wrap -> unwrap', async (t) => {
+  const unwrappedSource = await readFile(`${__dirname}/fixtures/unwrapped.js`);
+
+  t.is(await wrapUnwrap(unwrappedSource), unwrappedSource, 'no space');
+  t.is(
+    await wrapUnwrap(unwrappedSource, { spaceInside }),
+    unwrappedSource,
+    'space inside',
+  );
+});
+
+test('file - (wrap -> unwrap) x2', async (t) => {
+  const unwrappedSource = await readFile(`${__dirname}/fixtures/unwrapped.js`);
+
+  t.is(await wrapUnwrap2(unwrappedSource), unwrappedSource, 'no space');
+  t.is(
+    await wrapUnwrap2(unwrappedSource, { spaceInside }),
+    unwrappedSource,
+    'space inside',
+  );
+});
