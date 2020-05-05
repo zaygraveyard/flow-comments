3c31aac4e0f502e8ddea390cb83d0117e6c1de13
Ziad EL KHOURY HANNA <ziad.khoury.hanna@oceandatalab.com>
2020-05-05 02:24:51 +0200

    [unwrap] Cleanup

--- src/commands/unwrap.js
+++ src/commands/unwrap.js
@@ -8,21 +8,19 @@ export function unwrapMagicString(
   { spaceInside } = {},
 ) {
   for (const comment of ast.comments) {
-    if (comment.type !== 'CommentBlock') {
-      /*ignore line comments*/
-    } else if (
-      FLOW_COMMENT_REGEX.test(comment.value) ||
-      FLOW_SHORT_COMMENT_REGEX.test(comment.value)
+    if (
+      comment.type === 'CommentBlock' &&
+      (FLOW_COMMENT_REGEX.test(comment.value) ||
+        FLOW_SHORT_COMMENT_REGEX.test(comment.value))
     ) {
-      result.overwrite(
-        comment.start,
-        comment.end,
-        comment.value
-          .slice(FLOW_COMMENT_REGEX.lastIndex)
-          .replace(/ $/g, spaceInside ? '' : ' ')
-          .replace(/\*-\//g, '*/')
-          .replace(/\*-ESCAPED\//g, '*-/'),
-      );
+      let value = comment.value;
+
+      value = value.slice(FLOW_COMMENT_REGEX.lastIndex);
+      if (spaceInside) {
+        value = value.replace(/ $/g, '');
+      }
+      value = value.replace(/\*-\//g, '*/').replace(/\*-ESCAPED\//g, '*-/');
+      result.overwrite(comment.start, comment.end, value);
       FLOW_COMMENT_REGEX.lastIndex = 0;
     }
   }
